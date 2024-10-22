e3dba29b929ed13ead06d681fadee004a4ca6c31
Matia Mathias <matianyanzi@gmail.com>
2024-10-22 14:33:32 +0300

    removes unnecessary imports

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:nsamiziportal/Fragments/home.dart';
 import 'package:nsamiziportal/Fragments/profile.dart';
