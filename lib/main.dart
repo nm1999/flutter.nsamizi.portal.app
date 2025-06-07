f72d73be5f837508bb6703559dabf3f6c4cb1b17
matia mathias <nyanzimathiaz@gmail.com>
2025-06-07 20:10:34 +0300

    adds firebase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import './screens/splash_screen.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
