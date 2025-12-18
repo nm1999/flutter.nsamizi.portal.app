40d719e4733e18c81c6ac09d8e29fc97b8547bd1
matia mathias <nyanzimathiaz@gmail.com>
2025-12-18 20:29:46 +0300

    initial upgrade

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:nsamiziportal/main.dart';
+import 'package:nsamizi_portal/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
