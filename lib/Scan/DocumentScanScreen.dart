f50cb7e76ecd836fab813869514f057e2867dc22
matia mathias <nyanzimathiaz@gmail.com>
2026-02-19 14:15:49 +0300

    adds scanning code

---
+++ lib/Scan/DocumentScanScreen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_doc_scanner/flutter_doc_scanner.dart';
+
+class ScanDocument extends StatefulWidget {
+  const ScanDocument({super.key});
+
+  @override
+  State<ScanDocument> createState() => _ScanDocumentState();
+}
+
+class _ScanDocumentState extends State<ScanDocument> {
+  dynamic _scannedDocuments;
+
+  Future<void> scanDocumentAsPdf() async {
+    dynamic scannedDocuments;
+    try {
+      scannedDocuments =
+          await FlutterDocScanner().getScannedDocumentAsPdf(page: 4) ??
+          'Unknown platform documents';
+    } on PlatformException {
+      scannedDocuments = 'Failed to get scanned documents.';
+    }
+    print(scannedDocuments.toString());
+    if (!mounted) return;
+    setState(() {
+      _scannedDocuments = scannedDocuments;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          _scannedDocuments != null
+              ? Text(_scannedDocuments.toString())
+              : const Text("No Documents Scanned"),
+
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
+            child: ElevatedButton(
+              onPressed: () {
+                scanDocumentAsPdf();
+              },
+              child: const Text("Scan Documents As PDF"),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,6 +8,7 @@ import Foundation
 import firebase_auth
 import firebase_core
 import firebase_messaging
+import flutter_doc_scanner
 import path_provider_foundation
 import shared_preferences_foundation
 import sqflite_darwin
@@ -16,6 +17,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
+  FlutterDocScannerPlugin.register(with: registry.registrar(forPlugin: "FlutterDocScannerPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
