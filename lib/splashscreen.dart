2372528a12d4aa6440f027bcedf89c4e2a129699
matia mathias <nyanzimathiaz@gmail.com>
2026-06-01 22:46:43 +0300

    setting up the results

--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -16,7 +16,7 @@ class _SplashScreenState extends State<SplashScreen> {
 
   timingScreen() {
     Future.delayed(const Duration(seconds: 3), () {
-      
+      Get.off(ResultsView());
     });
   }
 
